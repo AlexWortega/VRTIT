b8789560f31c6202636193e45eb5bcd58f7dbfaa
Alex Wortega <alexdragannikolich@gmail.com>
2020-01-09 18:28:31 +0300

    gg

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public GameObject player;
+    public float dist;
+    NavMeshAgent nav;
+    public float MaxRadius = 3;
+    // Start is called- before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist > MaxRadius)
+        {
+            nav.enabled = true;
+            nav.SetDestination(player.transform.position);
+            gameObject.GetComponent<Animator>().SetTrigger("Shoot");
+        }
+        else
+        {
+            nav.enabled = false;
+            gameObject.GetComponent<Animator>().SetTrigger("Rifle");
+        }
+    }
+}
